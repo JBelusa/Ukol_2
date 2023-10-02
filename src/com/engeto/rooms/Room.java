5edbedf7e37cc7c23fd683c0f0e0454aafff9fb1
Jiří <belusa.j@gmail.com>
2023-10-02 23:51:19 +0200

    Nahrani kodu

---
+++ src/com/engeto/rooms/Room.java
@@ -0,0 +1,13 @@
+package com.engeto.rooms;
+
+public class Room {
+    private int roomNumber;
+    private int numberOfBeds;
+    private boolean hasBalcony;
+    private boolean  hasSeaView;
+
+
+
+
+
+}
